d1c9827a0811fe7e0b91b0e4997fa676f3b33e35
Ben Rosario <b.enn@icloud.com>
2025-03-10 01:25:04 -0700

    removed reference to vite.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from './components/RegistrationForm'
 
